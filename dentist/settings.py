f2418f97c4e9ee374b157ddf67701e37bd969326
umutkaraguler <70536338+umutkaraguler@users.noreply.github.com>
2020-09-01 17:05:07 +0300

    Update settings.py

--- dentist/settings.py
+++ dentist/settings.py
@@ -3,7 +3,7 @@ import django_heroku
 import dj_database_url
 from decouple import config
 
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
